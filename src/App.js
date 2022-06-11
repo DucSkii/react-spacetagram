41b2d3ea68c8988de1a0a75a743af1edbfc7f92d
DucSkii <ducvietdao@live.co.uk>
2022-06-11 16:39:09 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/*
+- Call data from NASA API
+- Card component, displaying image, title, date, description
+- Each card can be clicked into to expand and can be "Liked"
+- Include a loading state whilst gathering images
+- Create sharable links for each image
+- Date filter
+- Include local storaging
+*/
+
+const App = () => {
+  return <div className='App'></div>
 }
 
-export default App;
+export default App
