a52dd4fcc0160be512d7f935a6879c0d508c8bb3
DucSkii <ducvietdao@live.co.uk>
2022-06-12 18:31:15 +0100

    converted home to home page component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home'
 import './App.css'
-import axios from 'axios'
-import ImageCard from './components/ImageCard'
+// import axios from 'axios'
+// import ImageCard from './components/ImageCard'
 import Modal from './components/Modal'
 
 /*
@@ -19,68 +21,38 @@ UUID = DATE OF THE IMAGE
 */
 
 const App = () => {
-  const [imagesLoaded, setImagesLoaded] = useState(false)
-  const [images, setImages] = useState([])
-  // const [dateFilter, setDateFilter] = useState([0, 0])
-  const [likedImages, setLikedImages] = useState([])
   const [modal, setModal] = useState(null)
-  const standardImageCount = 5
 
-  useEffect(() => {
-    axios
-      .get(
-        `https://api.nasa.gov/planetary/apod?count=${standardImageCount}&thumbs=True&api_key=mDEcaIVsADm4IjeqttbyEn5W1eAD336EHkTbKlIt`
-      )
-      .then((res) => {
-        const imagesArray = res.data
-        imagesArray.forEach((imageData) => {
-          let title = imageData.title
-          let date = imageData.date
-          let description = imageData.explanation
-          let image = imageData.url
-          let imageObject = { title, date, description, image }
-          setImages((prevState) => [...prevState, imageObject])
-        })
-        setImagesLoaded(true)
-      })
-  }, [])
-
-  const likeImage = (image) => {
-    setLikedImages((prevState) => [...prevState, image])
-  }
-  const unLikeImage = (image) => {
-    setLikedImages(
-      likedImages.filter((likedImage) => likedImage.date !== image.date)
-    )
-  }
-console.log('likedImages', likedImages)
+  // {!imagesLoaded ? (
+  //   <div>Loading Images</div>
+  // ) : (
+  //   <div className='imageGallery'>
+  //     {/* <DateFilter /> */}
+  //     {images.map((imageObject, index) => {
+  //       return (
+  //         <div key={index}>
+  //           <ImageCard
+  //             title={imageObject.title}
+  //             date={imageObject.date}
+  //             description={imageObject.description}
+  //             image={imageObject.image}
+  //             likedImages={likedImages}
+  //             likeImage={likeImage}
+  //             unLikeImage={unLikeImage}
+  //             setModal={setModal}
+  //           />
+  //         </div>
+  //       )
+  //     })}
+  //   </div>
+  // )}
   return (
-    <div className={`App ${modal ? 'open' : ''}`}>
+    <div className='App'>
       <Modal modal={modal} setModal={setModal} />
       <h1 className='title'>Spacetagram</h1>
-      {!imagesLoaded ? (
-        <div>Loading Images</div>
-      ) : (
-        <div className='imageGallery'>
-          {/* <DateFilter /> */}
-          {images.map((imageObject, index) => {
-            return (
-              <div key={index}>
-                <ImageCard
-                  title={imageObject.title}
-                  date={imageObject.date}
-                  description={imageObject.description}
-                  image={imageObject.image}
-                  likedImages={likedImages}
-                  likeImage={likeImage}
-                  unLikeImage={unLikeImage}
-                  setModal={setModal}
-                />
-              </div>
-            )
-          })}
-        </div>
-      )}
+      <Routes>
+        <Route path='/' element={<Home setModal={setModal} />} />
+      </Routes>
     </div>
   )
 }
---
+++ src/Pages/Home/index.js
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import ImageCard from '../../components/ImageCard'
+import './index.css'
+
+const Home = ({ setModal }) => {
+  const [imagesLoaded, setImagesLoaded] = useState(false)
+  const [images, setImages] = useState([])
+  // const [dateFilter, setDateFilter] = useState([0, 0])
+  const [likedImages, setLikedImages] = useState([])
+  const standardImageCount = 5
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.nasa.gov/planetary/apod?count=${standardImageCount}&thumbs=True&api_key=mDEcaIVsADm4IjeqttbyEn5W1eAD336EHkTbKlIt`
+      )
+      .then((res) => {
+        const imagesArray = res.data
+        imagesArray.forEach((imageData) => {
+          let title = imageData.title
+          let date = imageData.date
+          let description = imageData.explanation
+          let image = imageData.url
+          let imageObject = { title, date, description, image }
+          setImages((prevState) => [...prevState, imageObject])
+        })
+        setImagesLoaded(true)
+      })
+  }, [])
+
+  const likeImage = (image) => {
+    setLikedImages((prevState) => [...prevState, image])
+  }
+  const unLikeImage = (image) => {
+    setLikedImages(
+      likedImages.filter((likedImage) => likedImage.date !== image.date)
+    )
+  }
+  return (
+    <>
+      {!imagesLoaded ? (
+        <div>Loading Images</div>
+      ) : (
+        <div className='imageGallery'>
+          {/* <DateFilter /> */}
+          {images.map((imageObject, index) => {
+            return (
+              <div key={index}>
+                <ImageCard
+                  title={imageObject.title}
+                  date={imageObject.date}
+                  description={imageObject.description}
+                  image={imageObject.image}
+                  likedImages={likedImages}
+                  likeImage={likeImage}
+                  unLikeImage={unLikeImage}
+                  setModal={setModal}
+                />
+              </div>
+            )
+          })}
+        </div>
+      )}
+    </>
+  )
+}
+
+export default Home
